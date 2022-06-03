e09a42d6dbc7c910b2ff7aa8fc5efcffb0221ae7
Jamal <jayjamal86@gmail.com>
2022-06-03 10:27:19 -0700

    first task

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -2,7 +2,9 @@
 #include <time.h>
 #include <stdio.h>
 /* more headers goes there */
-/* betty style doc for function main goes there */
+/*
+ * main - main block
+ * betty style doc for function main goes there */
 int main(void)
 {
 	int n;
