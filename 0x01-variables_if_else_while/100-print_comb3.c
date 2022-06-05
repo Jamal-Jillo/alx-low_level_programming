507d795acd6f5aa6d6afadbb7235e7d97f09cb8c
Jamal <jayjamal86@gmail.com>
2022-06-05 09:35:02 -0700

    betty update

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -2,11 +2,11 @@
 #include <stdlib.h>
 /**
  * main - main block
- * Description: program that will print all possible 
+ * Description: program that will print all possible
  * different combinations of digits
  * Return: 0
  */
-int main (void)
+int main(void)
 {
 	int c;
 	int d = 0;
@@ -20,18 +20,18 @@ int main (void)
 			{
 				putchar(48 + d);
 				putchar(48 + c);
-			
+
 				if (c + d != 17)
 				{
 					putchar(',');
 					putchar(' ');
 				}
 			}
-		
+
 			c++;
 		}
 		d++;
 	}
 	putchar('\n');
 	return (0);
-}	
+}
