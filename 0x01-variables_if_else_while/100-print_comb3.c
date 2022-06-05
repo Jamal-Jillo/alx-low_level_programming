31af9b28806220244ef82d1740826a976fdc6f5d
Jamal <jayjamal86@gmail.com>
2022-06-05 09:27:57 -0700

    created all the files first

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - main block
+ * Description: program that will print all possible 
+ * different combinations of digits
+ * Return: 0
+ */
+int main (void)
+{
+	int c;
+	int d = 0;
+
+	while (d < 10)
+	{
+		c = 0;
+		while (c < 10)
+		{
+			if (d != c && d < c)
+			{
+				putchar(48 + d);
+				putchar(48 + c);
+			
+				if (c + d != 17)
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+		
+			c++;
+		}
+		d++;
+	}
+	putchar('\n');
+	return (0);
+}	
