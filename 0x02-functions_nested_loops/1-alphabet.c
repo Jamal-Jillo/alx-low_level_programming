0cd1ecfb897e084352f9a11c91640e896ee32347
Jamal <jayjamal86@gmail.com>
2022-06-07 13:29:33 -0700

    created all the files first

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,18 +1,17 @@
 #include "main.h"
-#include <stdio.h>
 /**
  * main - main block
- * Decription: this code prints alphabets
- * Return: Always 0.
- */
+* Description: this code prints alphabets
+* Return: 0
+*/
 void print_alphabet(void);
 {
 	char c = 'a';
-
+	
 	while (c <= 'z')
 	{
-		_putchar(c);
+		_putchar('c');
 		c++;
 	}
-	return (0);
+	_putchar("\n");
 }
