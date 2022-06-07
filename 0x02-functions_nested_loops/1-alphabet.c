dc4e1057cc610fb750a60abd7a1b403e8d1bb11f
Jamal <jayjamal86@gmail.com>
2022-06-07 12:56:45 -0700

    submit

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,10 +1,11 @@
 #include "main.h"
+#include <stdio.h>
 /**
  * main - main block
  * Decription: this code prints alphabets
  * Return: Always 0.
  */
-void print_alphabet(void)
+void print_alphabet(void);
 {
 	char c = 'a';
 
