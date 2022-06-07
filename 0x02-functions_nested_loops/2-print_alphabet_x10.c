6c177bfea7662d146346941808af6942926ab82d
Jamal <jayjamal86@gmail.com>
2022-06-07 14:44:09 -0700

    x10 alphabet

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,25 @@
+#include "main.h"
+#include <unistd.h>
+/**
+ * main - main block
+ * Description: prints alphabet 10x
+ * Return: 0
+ */
+void print_alphabet_x10(void);
+{
+	int i = 0;
+	char c;
+
+	while (i < 10)
+	{
+		c = 'a';
+		while (c <= 'z')
+		{
+			_putchar(c);
+			c++;
+		}
+		_putchar('\n');
+		i++;
+	}
+	return (0);
+}
