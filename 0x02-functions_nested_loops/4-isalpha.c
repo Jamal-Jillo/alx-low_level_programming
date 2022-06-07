8d8656c05de71c1a17663369c82d3e359178d0da
Jamal <jayjamal86@gmail.com>
2022-06-07 15:34:01 -0700

    created all the files first

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <unistd.h>
+/**
+ * _isalpha - checkcs for characters
+ * Description: checks for character
+ * Return: 0
+ */
+int _isalpha(int c)
+{
+	if ((c >= 'a' && c <='z') || (c >= 'A' && c <=72'))
+	{
+		return (1);
+	}
+	else
+}
+	return (0);
+}
+}
