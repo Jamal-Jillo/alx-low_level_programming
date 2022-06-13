e666a7d275b5a362ca6d25738b993674b6b23590
Jamal <jayjamal86@gmail.com>
2022-06-13 02:26:55 -0700

    correction

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -4,6 +4,8 @@
  * main - main block
  * Description: function that takes a pointer to an int as parameter
  * Return: 0
+ * reset_to_98 - reset number to 98
+ * @n - pointer
  */
 void reset_to_98(int *n)
 {
