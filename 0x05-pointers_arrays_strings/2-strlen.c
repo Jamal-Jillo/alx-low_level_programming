5abcff086933fffe488edb5547af071089f1efcb
Jamal <jayjamal86@gmail.com>
2022-06-13 03:09:00 -0700

    string length project

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include "main.h"
+#include <string.h>
+/**
+ * _strlen - fucntion that returns the length of a string
+ * @s: character pointer
+ * return: void
+ */
+int _strlen(char *s)
+{
+	printf("length of a string: %ld", strlen(s));
+	return (0);
+}
