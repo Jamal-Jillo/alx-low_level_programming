0d5c0807898c93a5f12d100d3ebb1ac87b12082c
Jamal <jayjamal86@gmail.com>
2022-06-13 02:21:48 -0700

    battery st

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,11 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * main - main block
+ * Description: function that takes a pointer to an int as parameter
+ * Return: 0
+ */
+void reset_to_98(int *n)
+{
+	*n = 98
+}
