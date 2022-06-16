e9fe48171194721fe51df11918201d46e49ec862
Jamal <jayjamal86@gmail.com>
2022-06-16 09:48:19 -0700

    fixed betty errors

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -1,21 +1,21 @@
 #include <stdio.h>
 #include "main.h"
 /**
- * _strcmp - a function that compares strings
- *  @s1 - pointer
- * @s2 - pointer
- * Return: o
+ * _strcmp - function for comparing strings
+ * @s1: string one pointer
+ * @s2: strig two pointer
+ * Return: void
  */
 int _strcmp(char *s1, char *s2)
 {
-    int i;
-    
-    for (i = 0; s1[i != '\0'] || s2[i] != '\0'; i++)
-    {
-        if (s1[i] != s2[i])
-        {
-            return (s1[i] - s2[i]);
-        }
-    }
-    return (0);
+	int i;
+
+	for (i = 0; s1[i] != '\0' || s2[i] != '\0'; i++)
+	{
+		if (s1[i] != s2[i])
+		{
+			return (s1[i] - s2[i]);
+		}
+	}
+	return (0);
 }
