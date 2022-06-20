3dbc30023f9c79dea97dc373d84fd3fa9db0e33d
jay <jayjamal86@gmail.com>
2022-06-20 15:57:33 +0300

    strchr task

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * _strchr - function that locates a character in a string
+ * @s: string pointer
+ * @c: character that will be searched
+ * Return: void
+ */
+char *_strchr(char *s, char c)
+{
+	unsigned int i;
+
+	for (i = 0; s[i] >= '\0'; i++)
+	{
+		if (s[i] == 'c')
+		{
+			return (s + i);
+		}
+	}
+	return ('\0');
+}
