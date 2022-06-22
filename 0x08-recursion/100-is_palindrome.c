927c1def3ded3b86e2202a150e9362f294aff042
jay <jayjamal86@gmail.com>
2022-06-22 22:05:06 +0300

    sqrt recursion task

---
+++ 0x08-recursion/100-is_palindrome.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * is_palindrome - function that checks if a string is a palindrome
+ * @s: string
+ * Return: 0
+ */
+int is_palindrome(char *s)
+{
+	char *rev;
+
+
+}
--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -6,5 +6,20 @@
  */
 int _sqrt_recursion(int n)
 {
-	
+	return (square(n, 1));
+}
+/**
+ * square - finds the root
+ * @n: integer
+ * @result: result of root
+ * Return: 0
+ */
+int square(int n, int result)
+{
+	if (result * result == n)
+		return (result);
+	else if (result * result < n)
+		return (square(n, result + 1));
+	else
+		return (-1);
 }
