3c7d4eb00341d1805331a038b65c5a9e31d5643b
jay <jayjamal86@gmail.com>
2022-06-22 21:38:27 +0300

    6th recursion task

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,10 @@
+#include "main.h"
+/**
+ * _sqrt_recursion - function that returns sqrt of a number
+ * @n: interger
+ * Return: 0
+ */
+int _sqrt_recursion(int n)
+{
+	
+}
---
+++ 0x08-recursion/6-is_prime_number.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * is_prime_number - function that checks if int is prime number
+ * @n: interger
+ * Return: 0
+ */
+int is_prime_number(int n)
+{
+	if (n == 0 || n == 1)
+		return (1);
+	else if (n % 2 != 0)
+		return (1);
+	else
+		return (0);
+}
