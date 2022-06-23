3907ca8959bbc053b738ee1906f57fb82c460f7c
jay <jayjamal86@gmail.com>
2022-06-23 10:12:21 +0300

    palindorme recursion task

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <string.h>
 /**
  * is_palindrome - function that checks if a string is a palindrome
  * @s: string
@@ -6,7 +7,14 @@
  */
 int is_palindrome(char *s)
 {
-	char *rev;
+	int f = 0;
+	int l = strlen(s);
 
-
+	if (s[f] == s[l])
+		return (1);
+	else if (s[f] > s[l])
+		return (1);
+	else if (s[f] != s[l])
+		return (0);
+	return (is_palindrome(*s, f + 1, l - 1));
 }
