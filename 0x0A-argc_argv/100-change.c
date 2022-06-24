7cf6d40a0b4c3b832a49e23b25e54e15ddf25821
jay <jayjamal86@gmail.com>
2022-06-24 22:43:30 +0300

    correction

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -28,7 +28,7 @@ int main(int argc, char *argv[])
 		return (0);
 	}
 /* iterate through the coins array checking size */
-	for (i = 0; 1 < 5; i++)
+	for (i = 0; i < 5; i++)
 	{
 		while (cents >= coins[i])
 		{
