53ad784c856805a058e354f03909fbd745e923a9
jay <jayjamal86@gmail.com>
2022-06-24 15:07:11 +0300

    command line arguments

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - main function
+ * @argc: argument counter
+ * @argv: argument vector
+ * Return: 0,1
+ */
+int main(int argc, char *argv[])
+{
+	(void)argv;
+	printf("%d", argc);
+	return (0);
+}
