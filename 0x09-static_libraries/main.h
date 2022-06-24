3f0c9cd449a79444991df8c87ba0ca1d0c90fe06
jay <jayjamal86@gmail.com>
2022-06-24 21:39:29 +0300

    sasa marks ni kitu ya kukata mtu?

--- 0x09-static_libraries/main.h
+++ 0x09-static_libraries/main.h
@@ -1,5 +1,6 @@
 #ifndef MAIN_H
 #define MAIN_H
+#include <string.h>
 
 int _putchar(char c);
 int _islower(int c);
@@ -15,9 +16,9 @@ char *_strcat(char *dest, char *src);
 char *_strncat(char *dest, char *src, int n);
 char *_strncpy(char *dest, char *src, int n);
 int _strcmp(char *s1, char *s2);
-char *memset(char *s, char b, unsigned int n);
-char *memcpy(char *dest, char *src, unsigned int n);
-char *strchr(char *s, char c);
+char *_memset(char *s, char b, unsigned int n);
+char *_memcpy(char *dest, char *src, unsigned int n);
+char *_strchr(char *s, char c);
 unsigned int _strspn(char *s, char *accept);
 char *_strpbrk(char *s, char *accept);
 char *_strstr(char *haystack, char *needle);
