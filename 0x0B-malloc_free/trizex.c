e803d74badaa36dd40a1a9a0df5058c0a785051d
jay <jayjamal86@gmail.com>
2022-06-28 16:20:49 +0300

    malloc

---
+++ 0x0B-malloc_free/trizex.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * argstostr - function that concatenates all the arguments
+ * returns NULL if ac is 0 or av is NULL
+ * returns a pointer to a new string, or NULL if it fails
+ * each argument should be followed by a newline character ('\n')
+ * 
+ */
+char *argstostr(int ac, char **av)
+{
+	ss
+}
