6b4365fd905bd67fe7a756067ebe13a1bc87fadf
jay <jayjamal86@gmail.com>
2022-06-28 16:26:50 +0300

    malloc

--- 0x0B-malloc_free/100-argstostr.c
+++ 0x0B-malloc_free/100-argstostr.c
@@ -7,6 +7,10 @@
  * @ac: int
  * @av: char
  * Return: char
+ * c: count for the whole string
+ * i: index for the first array of strings
+ * j: index for the second array of strings
+ * ia: index for inserting new line character
  */
 char *argstostr(int ac, char **av)
 {
@@ -15,7 +19,7 @@ char *argstostr(int ac, char **av)
 
 	if (ac == 0)
 		return (NULL);
-
+/*finding the count of the 2D array*/
 	for (c = i = 0; i < ac; i++)
 	{
 		if (av[i] == NULL)
@@ -25,7 +29,7 @@ char *argstostr(int ac, char **av)
 			c++;
 		c++;
 	}
-
+/*allocating memory for the string*/
 	aout = malloc((c + 1) * sizeof(char));
 
 	if (aout == NULL)
@@ -33,7 +37,7 @@ char *argstostr(int ac, char **av)
 		free(aout);
 		return (NULL);
 	}
-
+/*add a new line after every word*/
 	for (i = j = ia = 0; ia < c; j++, ia++)
 	{
 		if (av[i][j] == '\0')
