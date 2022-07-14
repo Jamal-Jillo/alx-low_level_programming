d0f667abb8468f0d6cf781836b9ac90b9f07c1c0
jay <jayjamal86@gmail.com>
2022-07-14 20:38:48 +0300

    advanced

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,11 @@
+#include "lists.h"
+/**
+ * bmain - function that runs before main
+ * Return: void
+ */
+void bmain(void) __attribute__((constructor));
+void bmain(void)
+{
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
+}
