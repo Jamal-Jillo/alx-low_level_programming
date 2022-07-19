da08fa2d40613f9c440c550707a364601c811a2b
jay <jayjamal86@gmail.com>
2022-07-19 22:11:07 +0300

    reversing list

---
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -0,0 +1,43 @@
+#include "lists.h"
+#include <stdio.h>
+
+/**
+ * print_listint_safe - prints a linked list.
+ * @head: head of a list.
+ * Return: number of nodes in the list.
+ */
+size_t print_listint_safe(const listint_t *head)
+{
+	size_t nnodes = 0;
+	listp_t *hptr, *new, *add;
+
+	hptr = NULL;
+	while (head != NULL)
+	{
+		new = malloc(sizeof(listp_t));
+
+		if (new == NULL)
+			exit(98);
+
+		new->p = (void *)head;
+		new->next = hptr;
+		hptr = new;
+
+		add = hptr;
+
+		while (add->next != NULL)
+		{
+			add = add->next;
+			if (head == add->p)
+			{
+				printf("-> [%p] %d\n", (void *)head, head->n);
+				free_listp(&hptr);
+				return (nnodes);
+			}
+		}
+		nnodes++;
+		head = head->next;
+	}
+	free_listp(&hptr);
+	return (nnodes);
+}
