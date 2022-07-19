490b47bdec358dbb18fb07d4e11db5ee557a3aac
jay <jayjamal86@gmail.com>
2022-07-19 21:29:25 +0300

    inserting a new node

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -10,23 +10,39 @@
  */
 listint_t *insert_nodeint_at_index(listint_t *head, unsigned int idx, int n)
 {
-	listint_t *new_node = NULL;
-	listint_t *temp = head;
-	unsigned int i = 0;
+	listint_t *current;
+	listint_t *new;
+
+	if (head == NULL)
+		return (0);
+	new = malloc(sizeof(listint_t));
+	if (!new)
+		return (0);
+
+	new->next = NULL;
+	new->n = n;
 
 	if (idx == 0)
-		return (add_nodeint_end(&head, n));
-	while (temp != NULL)
 	{
-		if (i == idx - 1)
-		{
-			new_node = add_nodeint(&temp, n);
-			if (new_node == NULL)
-				return (NULL);
-			return (head);
-		}
-		temp = temp->next;
-		i++;
+		new->next = *head;
+		(*head) = new;
+		return (new);
 	}
-	return (NULL);
+
+	current = *head;
+
+	while (idx != 1)
+	{
+		current = current->next;
+		--idx;
+		if (current == NULL)
+		{
+			free(new);
+			return (NULL);
+		}
+	}
+	new->next = current->next;
+	current->next = new;
+
+	return (new);
 }
